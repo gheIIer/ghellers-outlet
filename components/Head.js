e1012230c724f8a5d1509ec1345be986d0668384
gheller <87474222+gheller3310@users.noreply.github.com>
2022-09-01 13:07:01 +0200

    Add files via upload

---
+++ components/Head.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+export default function HeadComponent() {
+  return (
+    <Head>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="theme-color" content="#000000" />
+
+      <title>Solana Pay Store</title>
+      <meta name="title" content="gheller's outlet" />
+      <meta name="description" content="Buy items on my store using Solana Pay!" />
+    </Head>
+  );
+}
---
+++ pages/api/createTransaction.js
@@ -0,0 +1,107 @@
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { clusterApiUrl, Connection, PublicKey, Transaction } from "@solana/web3.js";
+import { createTransferCheckedInstruction, getAssociatedTokenAddress, getMint } from "@solana/spl-token";
+import BigNumber from "bignumber.js";
+import products from "./products.json";
+
+// Make sure you replace this with your wallet address!
+const usdcAddress = new PublicKey("Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr")
+// Mainnet: EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v
+// Devnet: Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr
+const sellerAddress = 'CwtZTbZZz7NEayq3heQF7PDX3tCRbaqSmtwDzMVz8RS9'
+const sellerPublicKey = new PublicKey(sellerAddress);
+
+const createTransaction = async (req, res) => {
+  try {
+    // Extract the transaction data from the request body
+    const { buyer, orderID, itemID } = req.body;
+
+    // If we don't have something we need, stop!
+    if (!buyer) {
+      res.status(400).json({
+        message: "Missing buyer address",
+      });
+    }
+
+    if (!orderID) {
+      res.status(400).json({
+        message: "Missing order ID",
+      });
+    }
+
+    // Fetch item price from products.json using itemID
+    const itemPrice = products.find((item) => item.id === itemID).price;
+
+    if (!itemPrice) {
+      res.status(404).json({
+        message: "Item not found. please check item ID",
+      });
+    }
+    
+    // Convert our price to the correct format
+    const bigAmount = BigNumber(itemPrice);
+    const buyerPublicKey = new PublicKey(buyer);
+
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = clusterApiUrl(network);
+    const connection = new Connection(endpoint);
+
+    // A blockhash is sort of like an ID for a block. It lets you identify each block.
+    const buyerUsdcAddress = await getAssociatedTokenAddress(usdcAddress, buyerPublicKey);
+    const shopUsdcAddress = await getAssociatedTokenAddress(usdcAddress, sellerPublicKey);
+    const { blockhash } = await connection.getLatestBlockhash("finalized");
+    
+    // We're getting the mint address of the token we want to transfer
+    const usdcMint = await getMint(connection, usdcAddress);
+
+    // The first two things we need - a recent block ID 
+    // and the public key of the fee payer 
+    const tx = new Transaction({
+      recentBlockhash: blockhash,
+      feePayer: buyerPublicKey,
+    });
+
+    // Here we're creating a different type of transfer instruction
+    const transferInstruction = createTransferCheckedInstruction(
+        buyerUsdcAddress, 
+        usdcAddress,     // This is the address of the token we want to transfer
+        shopUsdcAddress, 
+        buyerPublicKey, 
+        bigAmount.toNumber() * 10 ** (await usdcMint).decimals, 
+        usdcMint.decimals // The token could have any number of decimals
+      );
+
+    // We're adding more instructions to the transaction
+    transferInstruction.keys.push({
+      // We'll use our OrderId to find this transaction later
+      pubkey: new PublicKey(orderID), 
+      isSigner: false,
+      isWritable: false,
+    });
+
+    tx.add(transferInstruction);
+  
+    // Formatting our transaction
+    const serializedTransaction = tx.serialize({
+      requireAllSignatures: false,
+    });
+    const base64 = serializedTransaction.toString("base64");
+
+    res.status(200).json({
+      transaction: base64,
+    });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({ error: "error creating transaction" });
+    return;
+  }
+};
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    createTransaction(req, res);
+  } else {
+    res.status(405).end();
+  }
+}
